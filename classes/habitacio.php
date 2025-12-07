76f7d711ca1e9bdbbf8cc45bc5218af83eff73b3
samuel <sga18@alumnes.udl.cat>
2025-12-07 17:26:42 +0100

    Refactor llistatHabitacions method for clarity

--- classes/habitacio.php
+++ classes/habitacio.php
@@ -27,35 +27,34 @@ class Habitacio
     }
 
     public function llistatHabitacions()
+{
+    $res = array();
+    $this->abd->connectarBD();
+
+    $sql = "SELECT codi, tipus, preuNit, descripcio 
+            FROM habitacio 
+            WHERE codi <> '0'";
+
+    if ($this->abd->consultaSQL($sql))
     {
-        $res = array();
-        $this->abd->connectarBD();
-        $sql = "SELECT codi, tipus, preuNit, descripcio FROM habitacio WHERE codi <> '0'";
-    
-        if ($this->abd->consultaSQL($sql))
+        $fila = $this->abd->consultaFila();
+        $i = 0;
+        while ($fila != null)
         {
+            $res[$i]["codi"] = $this->abd->consultaDada("codi");
+            $res[$i]["tipus"] = $this->abd->consultaDada("tipus");
+            $res[$i]["preuNit"] = $this->abd->consultaDada("preuNit");
+            $res[$i]["descripcio"] = $this->abd->consultaDada("descripcio");
+
+            $i++;
             $fila = $this->abd->consultaFila();
-            $i = 0;
-            while ($fila != null)
-            {
-                $res[$i]["codi"] = $this->abd->consultaDada("codi");
-                $res[$i]["tipus"] = $this->abd->consultaDada("tipus");
-                $res[$i]["preuNit"] = $this->abd->consultaDada("preuNit");
-                $res[$i]["descripcio"] = $this->abd->consultaDada("descripcio");
-                
-                $i++;
-                $fila = $this->abd->consultaFila();
-            }
-            $this->abd->tancarConsulta();
         }
-        else
-        {
-            echo "Error en la consulta: " . $this->abd->missatgeError();
-        }
-    
-        $this->abd->desconnectarBD();
-        return $res; 
+        $this->abd->tancarConsulta();
     }
+    $this->abd->desconnectarBD();
+
+    return $res; 
+}
 
 /*******************************************************************************/
 
